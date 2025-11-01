7d790c5dc9d9c202027a5810ea0dfde1c44e074b
Olamide Aworetan <aworetanolamide@gmail.com>
2025-11-01 23:22:54 +0100

    Add team member details to simple_code.py
    Added a list of team members with their details including name, Slack username, country, hobby, affiliation, and DNA sequence.

---
+++ simple_code.py
@@ -0,0 +1,70 @@
+team_alanin = [
+    {
+    "Name" : "Aworetan Olamide",
+     "Slack_Username" : "Olamide_etan",
+     "Country" : "Nigeria",
+     "Hobby" : "Swimming",
+     "Affiliation" : "Ekiti State University",
+     "DNA seq" : "ATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGAAACATTTTCAGACCTAT"
+},
+     {
+    "Name" : "Faith Omondi",
+     "Slack_Username" : "Faith",
+     "Country" : "Kenya",
+     "Hobby" : "Drawing",
+     "Affiliation" : "University of Nairobi",
+     "DNA seq" : "ATGGCCCTGTGGATGCGCCTCCTGCCCCTGCTGGCGCTGCTGGCCCTCTGGGG ACCTGACCCAG"
+},
+     {
+    "Name" : "Shreya Venugopalan Omesh Chendilkumar ",
+     "Slack_Username" : "Shreya",
+     "Country" : "India",
+     "Hobby" : "Music",
+     "Affiliation" : "Anna University",
+     "DNA seq" : "AGTGCAGACGCGGCTCCTAGCGGATGGGTGCTATTGTGAGGCGGTTGTAGAAGGTATGAGGAGGCTGT"
+},
+     {
+    "Name" : "Taiwo Bankole",
+     "Slack_Username" : "Taiwo Bankole",
+     "Country" : "Nigeria",
+     "Hobby" : "Reading",
+     "Affiliation" : "University of Maryland",
+     "DNA seq" : "ACAGCCCACAAAATTCCACCTGCTCACAGGTTGGCTGGCTCGACCCAGGTGGTGTCCCCTGCTCTGAGCC"
+},
+     {
+    "Name" : "Adeyeye Daniel",
+     "Slack_Username" : "Adeyeye Daniel",
+     "Country" : "Nigeria",
+     "Hobby" : "Writing",
+     "Affiliation" : "Mohammed VI Polytechnic University ",
+     "DNA seq" : "ATGCGTACGTTAGCCTGACCGGATCGTTAAGGCTGATCGGCAATGCCGTTGATCCTGAGCTTGGACGATGCT"
+},
+     {
+    "Name" : "Ofosaren Favour oghenevwerhie",
+     "Slack_Username" : "Ofosaren Favour",
+     "Country" : "Nigeria",
+     "Hobby" : "Dancing",
+     "Affiliation" : "Delta state university",
+     "DNA seq" : "ATGCGTACCTGAACTGCTTAGGCTTACGGAATCGTAA"
+},
+     {
+    "Name" : "Oyiyechukwu Elizabeth Chikelu ",
+     "Slack_Username" : "Elizabeth Chikelu",
+     "Country" : "Nigeria",
+     "Hobby" : "Writing",
+     "Affiliation" : "Enugu State University of Science and Technology, Nigeria",
+     "DNA seq" : "AGCTCCCGGCCAAGCCAGCACCATGGCCAGATACCGATGCTGCCGCAGCAAAAGCAGGAGCAGATGCCGC"
+},
+     {
+    "Name" : "Jana Zaki",
+     "Slack_Username" : "Jana Zaki",
+     "Country" : "UK",
+     "Hobby" : "Drawing",
+     "Affiliation" : "UCL",
+     "DNA seq" : "ATGGAGGAGCCGCAGTCAGATCCTAGCGTCGAGCCCCCTCTGAGTCAGGATAT"
+}
+
+]
+
+
+print(team_alanin)
